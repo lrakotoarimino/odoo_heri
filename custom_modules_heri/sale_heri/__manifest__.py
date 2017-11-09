44556d4e526f167b9e30ef06aa5961f2ecea9540
Ando <arazanajatovo@ingenosya.mg>
2017-11-09 15:15:19 +0000

    Masquer le sous-menu Bon de sortie dans le menu Imprimer stock.picking

---
+++ custom_modules_heri/sale_heri/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale module for HERi',
+    'version': '1.0',
+    'category': 'sale',
+    'sequence': 0,
+    'description': """Module des ventes pour Heri""",
+    'website': 'https://www.beheri.com',
+    'depends': ['sale'],
+    'data': [
+#                 'views/product_view.xml',
+#                 'views/res_partner_view.xml',
+             ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
