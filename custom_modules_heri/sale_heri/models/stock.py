24b0cad0b826f60dd099c5f35a95c68f3c22722b
srazafindrakoto <srazafindrakoto@ingenosya.mg>
2017-11-23 16:59:45 +0000

    commit apres Ando 23/11/2017 16:59

--- custom_modules_heri/sale_heri/models/stock.py
+++ custom_modules_heri/sale_heri/models/stock.py
@@ -4,8 +4,9 @@ from odoo import fields, models, api
 
 class StockPickingHeri(models.Model):
     _inherit = 'stock.picking'   
-    
+
     date_arrivee_relle = fields.Datetime(string="Date d'arrivée réelle des matériels")  
+
     def aviser_magasinier_tiers(self):
         self.action_confirm()
         self.write({'state':'attente_magasinier'})    
