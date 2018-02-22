2b642ccd2295ae01e3d9ab27111a062953158cda
Ando <arazanajatovo@ingenosya.mg>
2018-02-22 10:46:23 +0000

    Initialisation du champ account_type dans account.invoice

--- custom_modules/sale_advanced_heri/models/invoice.py
+++ custom_modules/sale_advanced_heri/models/invoice.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
-
+    
+    
+    @api.model
+    def _default_account_type(self):
+        if self._context.get('invoice_type', False):
+            return self._context.get('invoice_type', False)
+    
     kiosk_id = fields.Many2one('stock.location', string='Kiosk *')
     date_start = fields.Datetime(string='Start date')
     date_end = fields.Datetime(string='End date')
-    invoice_type = fields.Selection([('rental', 'Rental'), ('sale', 'Sale'), ('loss', 'Loss')], string="Type invoice")
+    invoice_type = fields.Selection([('rental', 'Rental'), ('sale', 'Sale'), ('loss', 'Loss')], string="Type invoice", default=_default_account_type)
