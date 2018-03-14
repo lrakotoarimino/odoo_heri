448c1dcee3d3057706a4301e6d5ae8d3770eeb5e
Ando <arazanajatovo@ingenosya.mg>
2018-03-14 10:44:29 +0000

    Correction place pu_discount purchase_heri

--- custom_modules/sale_advanced_heri/models/sale.py
+++ custom_modules/sale_advanced_heri/models/sale.py
@@ -5,3 +5,14 @@ from odoo import api, fields, models
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+    
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+    
+    # redefinition
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
+        invoice.invoice_type = 'sale'
+        return invoice
